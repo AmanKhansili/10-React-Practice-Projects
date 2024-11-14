29c1e1a78363349d00c419988dab1c7e5c18bc19
Aman Khansili <amankhansilee@gmail.com>
2024-11-14 18:22:28 +0530

    project-2 completed

--- project-2/src/App.jsx
+++ project-2/src/App.jsx
@@ -1,13 +1,18 @@
 import { useState } from "react";
 import "./App.css";
+import Navigation from "./components/Navigation";
+import ContectHeader from "./components/ContectHeader";
+import ContectForm from "./components/ContectForm";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <>
-      <h1>Welcome to Project-2</h1>
-    </>
+    <div className="container w-full h-screen px-20 py-5">
+      <Navigation />
+      <ContectHeader />
+      <ContectForm />
+    </div>
   );
 }
 
---
+++ project-2/src/components/ContectHeader.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const ContectHeader = () => {
+  return (
+    <div className="px-20 py-5 mt-[4rem]">
+      <h1 className="font-bold text-[3rem]">CONTACT US</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis, voluptatibus officiis
+        soluta, deserunt ea consectetur velit, sapiente illum delectus dolorem architecto vitae
+        cupiditate eos aut quisquam? Eum illum voluptates, officia, cum numquam, itaque quaerat
+        nobis et aut odio beatae repudiandae iste. Excepturi reprehenderit harum dolorum. Nobis
+        aliquam alias aspernatur possimus!
+      </p>
+    </div>
+  );
+};
+
+export default ContectHeader;
