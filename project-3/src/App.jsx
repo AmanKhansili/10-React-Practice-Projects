e965eb506e31f0e14703af0463640dfacd2f1102
Aman Khansili <amankhansilee@gmail.com>
2024-11-15 19:40:13 +0530

    Adding search functionality and project is completed

--- project-3/src/App.jsx
+++ project-3/src/App.jsx
@@ -5,11 +5,7 @@ import Pokemon from "./Pokemon";
 function App() {
   const [count, setCount] = useState(0);
 
-  return (
-    <>
-      <Pokemon />
-    </>
-  );
+  return <Pokemon />;
 }
 
 export default App;
--- project-3/src/Pokemon.jsx
+++ project-3/src/Pokemon.jsx
@@ -7,7 +7,9 @@ const Pokemon = () => {
   const [pokemon, setPokemon] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
-  const API = "https://pokeapi.co/api/v2/pokemon?limit=10";
+  const [search, setSearch] = useState("");
+
+  const API = "https://pokeapi.co/api/v2/pokemon?limit=500";
 
   const fetchPokemon = async () => {
     try {
@@ -34,6 +36,11 @@ const Pokemon = () => {
     fetchPokemon();
   }, []);
 
+  //Search Functionality
+  const searchData = pokemon.filter((curPokemon) =>
+    curPokemon.name.toLowerCase().includes(search.toLowerCase())
+  );
+
   if (loading) {
     return (
       <div>
@@ -48,21 +55,29 @@ const Pokemon = () => {
       </div>
     );
   }
+
   return (
-    <>
-      <section className="container">
-        <header>
-          <h1>Let's Catch Pokemon</h1>
-        </header>
-        <div>
-          <ul className="cards">
-            {pokemon.map((curPokemon) => (
-              <PokemonCards key={curPokemon.id} pokemonData={curPokemon} />
-            ))}
-          </ul>
-        </div>
-      </section>
-    </>
+    <section className="container">
+      <header>
+        <h1>Let's Catch Pokemon</h1>
+      </header>
+      <div className="pokemon-search">
+        <input
+          type="text"
+          name="search"
+          placeholder="Search Pokemon"
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        />
+      </div>
+      <div>
+        <ul className="cards">
+          {searchData.map((curPokemon) => (
+            <PokemonCards key={curPokemon.id} pokemonData={curPokemon} />
+          ))}
+        </ul>
+      </div>
+    </section>
   );
 };
 
