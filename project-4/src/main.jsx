1535f31a2704433f8b31c603cc6967da775fa4f2
Aman Khansili <amankhansilee@gmail.com>
2024-11-16 12:44:59 +0530

    Landing page completed

--- project-4/src/main.jsx
+++ project-4/src/main.jsx
@@ -1,10 +1,23 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import StartGame from "./components/StartGame.jsx";
+import GamePlay from "./components/GamePlay.jsx";
 
-createRoot(document.getElementById('root')).render(
+const route = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/game",
+    element: <GamePlay />,
+  },
+]);
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={route} />
+  </StrictMode>
+);
