588980e5b7b2730216334239167bcdd848ea6b04
Aman Khansili <amankhansilee@gmail.com>
2024-11-18 23:30:53 +0530

    project-5

--- README.md
+++ README.md
@@ -40,7 +40,14 @@ Happy coding! 🎉
 
 <img width="1154" alt="image" src="https://user-images.githubusercontent.com/50476777/236659200-8ba6c2dc-8815-46ed-bf3e-f873da7a6064.png">
 
-
 Figma Design URL - https://www.figma.com/file/rephrU2FVgN8MFz6XhnP51/Learn-React-with-10-Projects?type=design&node-id=21-8&t=orp3GQEAXhmtQcgG-0
 
+---
 
+# PROJECT 5 - FIREBASE CONTACT APP
+
+![React 8](https://github.com/anshuopinion/React-10-Projects/assets/50476777/2712d5c5-e650-43b9-a177-92401a5a051c)
+
+Figma Design URL - [https://www.figma.com/file/rephrU2FVgN8MFz6XhnP51/Learn-React-with-10-Projects?type=design&node-id=0-1&t=orp3GQEAXhmtQcgG-0](https://www.figma.com/file/rephrU2FVgN8MFz6XhnP51/Learn-React-with-10-Projects?type=design&node-id=864-52&t=wuNRaDLVHqH5cAaK-0)
+
+---
--- project-5/src/App.jsx
+++ project-5/src/App.jsx
@@ -1,16 +1,26 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import Navbar from "./components/Navbar";
+import { FiSearch } from "react-icons/fi";
+import { AiFillPlusCircle } from "react-icons/ai";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <>
+    <div className="mx-auto max-w-[370px] px-4">
       <Navbar />
-    </>
+      <div className="flex gap-2">
+        <div className="relative flex flex-grow items-center">
+          <FiSearch className="absolute ml-1 text-3xl text-white" />
+          <input
+            type="text"
+            className="h-10 flex-grow rounded-md border border-white bg-transparent pl-9 text-white"
+          />
+        </div>
+        <AiFillPlusCircle className="cursor-pointer text-5xl text-white" />
+      </div>
+    </div>
   );
 }
 
